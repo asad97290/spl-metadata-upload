e47dd3b28c5a8a54bf2e742eb65686cd749303d0
Asadullah khan <asadmuhammad427@gmail.com>
2025-03-06 12:20:30 +0500

    initial commit

--- mpl_metadata.ts
+++ mpl_metadata.ts
@@ -27,7 +27,7 @@ async function main(){
     const ourMetadata = { // TODO change those values!
         name: "Silly Dragon Token", 
         symbol: "SDT",
-        uri: "https://raw.githubusercontent.com/loopcreativeandy/video-tutorial-resources/main/metadata.json",
+        uri: "https://raw.githubusercontent.com/asad97290/spl-metadata-upload/main/metadata.json",
     }
     const onChainData = {
         ...ourMetadata,
